0039c38d289ccbf46b3701313115e0234232141e
Jorge J. Riera <jorge.riera9@gmail.com>
2017-06-18 22:53:16 -0300

    inicializando nigromancer.com

---
+++ content/firstpost.md
@@ -0,0 +1,6 @@
+Title: Nigromancer
+Date: 2017-06-18 23:33
+Category: About 
+Tags: nigromancer, Mordor
+
+Buenas noches Mordor!
---
+++ pelicanconf.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jorge J. Riera'
+SITENAME = u'nigromancer'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Argentina/Jujuy'
+
+DEFAULT_LANG = u'es'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+THEME = 'lazystrap'
+
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+ARTICLE_URL = 'blog/{slug}'
+ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
+ARCHIVES_SAVE_AS = 'archives/index.html'
+PAGINATED_DIRECT_TEMPLATES = ('archives',)
+PAGINATION_PATTERNS = ((1, '{base_name}/', '{base_name}/index.html'), (2, '{base_name}/page{number}/', '{base_name}/page{number}/index.html'))
+
+#All theme text was moved to template variables:
+
+ARCHIVES_TITLE = "Archivos"
+COMMENTS_TITLE = "Comentarios"
+FEED_TITLE = "RSS"
+RECENT_POSTS_TITLE = "Escrituras recientes"
+SEARCH_TITLE = "Buscar"
+PREV_TITLE = "Previo"
+NEXT_TITLE = "Siguiente"
+TAG_PAGE_TITLE = "Post tagged by"
+
+#Other theme variables:
+
+SITENAME = "Nigromancer"
+SITESUBTITLE = "Escrituras de Mordor"
+SITEKEYWORDS = "GNU Linux Debian Mordor Music Cine Geek Bass"
+SITEURL = "http://nigromancer.com"
+SOCIAL = (('twitter', 'http://twitter.com/nigrobyte'),)
+GITHUB_USERNAME = "nigromancer"
+BLOG_START_YEAR = "2017"
+COMMENT_SYSTEM_ID = ""
